86d4f4dcc6741d5d64f4feed19da5894004b2b15
idrismikayil <idrisrm@code.edu.az>
2022-01-12 18:45:21 +0400

    edit

---
+++ ASP Homework Task 1/ASP/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "Index";
+            return view;
+
+        }
+
+        public ActionResult About()
+        {
+            return new ViewResult { ViewName = "About" };
+        }
+
+        public ActionResult Contact()
+        {
+            return new ViewResult { ViewName = "Contact" };
+        }
+    }
+}
